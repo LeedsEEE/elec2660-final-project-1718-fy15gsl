641f54bb3e1e5c0a1098efd9a009ce4c78298b55
Guido Longo Figueroa [fy15gsl] <fy15gsl@eentpc98.leeds.ac.uk>
2017-12-06 21:47:31 +0000

    Correction: suggestions need to change when user goes back to home screen and press again "GO"

--- Project/ViewController1.m
+++ Project/ViewController1.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    // need new method
     self.suggestion = [[Suggestions alloc] init];
     self.Suggestion1 = [[Module alloc] init];
     self.Suggestion2 = [[Module alloc] init];
